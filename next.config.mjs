884183152de78f509b51b05e9c6bebe38e2c931c
Winston Dennis-Masi <winstondennismasi@gmail.com>
2024-08-11 04:51:08 -0400

    fixed post error mismatch with data being recieved, implemented light-weight vectorizer and vector strore to replace HuggingsFaceEmbeddings cause of onnx incompatabilty

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          fs: false,
+          net: false,
+          tls: false,
+        };
+      }
+      config.externals.push({
+        'onnxruntime-node': 'commonjs onnxruntime-node',
+      });
+      return config;
+    },
+  };
+  
+  export default nextConfig;
