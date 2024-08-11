78a1e383dc29e010f43c4aee3e2de79c562f040b
Tingly <pi@piprograms.io>
2024-08-11 06:57:11 -0400

    Updated firebase.js

--- firebase.js
+++ firebase.js
@@ -25,13 +25,13 @@ import { getAuth, GoogleAuthProvider,onAuthStateChanged, signInWithPopup, signOu
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9GnClWA2oUINAWvUE8qMEP0gOkgfvxzM",
-  authDomain: "interview-bot-d1442.firebaseapp.com",
-  projectId: "interview-bot-d1442",
-  storageBucket: "interview-bot-d1442.appspot.com",
-  messagingSenderId: "293353353891",
-  appId: "1:293353353891:web:e7508a95a7bce947814516",
-  measurementId: "G-746NZMPNSP"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
